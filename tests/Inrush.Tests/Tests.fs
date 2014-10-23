bf60938659120d88f5e4f5b3e9e88283aa068c48
mavnn <michael@mavnn.co.uk>
2014-10-23 16:22:51 +0100

    Fix tests, working build.

--- tests/Inrush.Tests/Tests.fs
+++ tests/Inrush.Tests/Tests.fs
@@ -1,10 +1,16 @@
 module Inrush.Tests
 
 open Inrush
+open Inrush.QueryTypes
 open NUnit.Framework
 
 [<Test>]
-let ``hello returns 42`` () =
-  let result = Library.hello 42
-  printfn "%i" result
-  Assert.AreEqual(42,result)
+let ``Sanity check`` () =
+    let select =
+        SelectStatement({ Columns = 
+                              Columns [ Sum(ColumnName "value")
+                                        Name(ColumnName "bob") ]
+                          Series = Single "test"
+                          Where = Some (Equal(ColumnName "bob", String "bob"))
+                          GroupBy = Some ([TimeGroup(Days 1);ColumnGroup(ColumnName "bob")] , None) })
+    Assert.AreEqual("SELECT SUM(value), bob FROM \"test\" WHERE bob = \"bob\" GROUP BY time(1d), bob", show select)
